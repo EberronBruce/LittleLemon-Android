c5aa1227fcca9714ad9ae1501363d28f5ae5f310
Bruce <bruce@redravencomputing.com>
2023-11-20 02:57:14 -0600

    Adjust Header Added Hero to home

--- app/src/main/java/com/redravencomputing/littlelemonandroid/views/Home.kt
+++ app/src/main/java/com/redravencomputing/littlelemonandroid/views/Home.kt
@@ -1,17 +1,40 @@
 package com.redravencomputing.littlelemonandroid.views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.redravencomputing.littlelemonandroid.R
 import com.redravencomputing.littlelemonandroid.ui.theme.LittleLemonAndroidTheme
+import com.redravencomputing.littlelemonandroid.ui.theme.LittleLemonLightGrey
+import com.redravencomputing.littlelemonandroid.ui.theme.LittleLemonYellow
 import com.redravencomputing.littlelemonandroid.viewModels.Profile
 
 @Composable
@@ -24,9 +47,89 @@ fun HomeScreen(navController: NavHostController) {
 				navController.navigate(Profile.route)
 			}
 	) {
-		Text(text = "Home Screen")
+		Header(home = true, navController = navController)
+		Hero()
 	}
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Hero() {
+	Column(
+		modifier = Modifier
+			.fillMaxWidth()
+			.background(MaterialTheme.colorScheme.primary)
+	) {
+		Text(
+			text = "Little Lemon",
+			color = LittleLemonYellow,
+			fontSize = 40.sp,
+			fontWeight = FontWeight.SemiBold,
+			modifier = Modifier
+				.padding(start = 20.dp, top = 8.dp)
+		)
+		Text(
+			text = "Chicago",
+			color = LittleLemonLightGrey,
+			fontSize = 30.sp,
+			fontWeight = FontWeight.Medium,
+			modifier = Modifier
+				.padding(start = 20.dp)
+		)
+		Row(
+			modifier = Modifier
+				.fillMaxWidth()
+				.padding(10.dp)
+		) {
+			Text(
+				text = "We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.",
+				color = LittleLemonLightGrey,
+				modifier = Modifier
+					.fillMaxWidth(0.6f)
+					.padding(10.dp)
+			)
+			Image(
+				painter = painterResource(id = R.drawable.hero_image),
+				contentDescription = "Hero Image",
+				contentScale = ContentScale.FillWidth,
+				modifier = Modifier
+					.sizeIn(
+						minWidth = 100.dp,
+						minHeight = 100.dp,
+						maxHeight = 150.dp,
+						maxWidth = 150.dp
+					)
+//					.size(150.dp)
+//					.height(150.dp)
+					.padding(10.dp)
+					.clip(RoundedCornerShape(16.dp))
+			)
+		}
+
+		TextField(
+			value = "",
+			onValueChange = {},
+			leadingIcon = {
+				Icon(
+					imageVector = Icons.Default.Search,
+					contentDescription = "Search Icon")
+			},
+			placeholder = { Text("Search...")},
+			keyboardOptions = KeyboardOptions(
+				imeAction = ImeAction.Search
+			),
+			modifier = Modifier
+				.fillMaxWidth()
+				.padding(10.dp),
+			colors =  TextFieldDefaults.textFieldColors(containerColor = MaterialTheme.colorScheme.background),
+			shape = RoundedCornerShape(16.dp)
+		)
+
+	}
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
@@ -34,5 +137,13 @@ fun HomePreview() {
 	LittleLemonAndroidTheme {
 		val navController = rememberNavController()
 		HomeScreen(navController)
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeroPreview() {
+	LittleLemonAndroidTheme {
+		Hero()
 	}
 }
--- app/src/main/java/com/redravencomputing/littlelemonandroid/views/SharedComposables.kt
+++ app/src/main/java/com/redravencomputing/littlelemonandroid/views/SharedComposables.kt
@@ -2,38 +2,103 @@ package com.redravencomputing.littlelemonandroid.views
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.redravencomputing.littlelemonandroid.R
+import com.redravencomputing.littlelemonandroid.ui.theme.LittleLemonAndroidTheme
+import com.redravencomputing.littlelemonandroid.ui.theme.LittleLemonGreen
 import com.redravencomputing.littlelemonandroid.ui.theme.LittleLemonLightGrey
+import com.redravencomputing.littlelemonandroid.ui.theme.LittleLemonYellow
+import com.redravencomputing.littlelemonandroid.viewModels.Profile
 
 @Composable
-fun Header() {
+fun Header(navController: NavHostController? = null, home: Boolean = false, profile : Boolean = false) {
 	val logo = painterResource(id = R.drawable.logo)
-	Box(
+	val profileImage = painterResource(id = R.drawable.ic_profile)
+	val backImage = painterResource(id = R.drawable.ic_arrow_back)
+
+	Row(
 		modifier = Modifier
 			.fillMaxWidth()
 			.height(90.dp)
 			.background(MaterialTheme.colorScheme.background)
 			.padding(16.dp),
-		contentAlignment = Alignment.Center
+		horizontalArrangement = Arrangement.Center,
+		verticalAlignment = Alignment.CenterVertically
 	) {
+
+		if (profile) {
+			Box(
+				modifier = Modifier
+					.size(40.dp) // Adjust size as needed
+					.background(shape = CircleShape, color = LittleLemonGreen),
+				contentAlignment = Alignment.Center
+			) {
+				Image(
+					painter = backImage,
+					contentDescription = "Profile Image",
+					colorFilter = ColorFilter.tint(LittleLemonYellow),
+					modifier = Modifier
+						.size(40.dp) // Adjust size as needed
+						.padding(8.dp)
+						.clickable {
+							navController?.popBackStack()
+						}
+				)
+			}
+		}
+
+		Spacer(modifier = Modifier.weight(if (home) 2f else 1f))
+
 		Image(
 			painter = logo,
 			contentDescription = "Logo",
 			modifier = Modifier
 				.padding(5.dp)
 		)
+
+		Spacer(modifier = Modifier.weight(if (profile) 2f else 1f))
+
+		if (home) {
+			Box(
+				modifier = Modifier
+					.size(50.dp) // Adjust size as needed
+					.background(shape = CircleShape, color = LittleLemonGreen),
+				contentAlignment = Alignment.Center
+			) {
+				Image(
+					painter = profileImage,
+					contentDescription = "Profile Image",
+					colorFilter = ColorFilter.tint(LittleLemonYellow),
+					modifier = Modifier
+						.size(50.dp) // Adjust size as needed
+						.padding(8.dp)
+						.clickable {
+							navController?.navigate(Profile.route)
+						}
+				)
+			}
+		}
+
 	}
 }
 
@@ -51,5 +116,13 @@ fun ScreenTitle(title: String) {
 			fontSize = 24.sp,
 			color = LittleLemonLightGrey
 		)
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderPreview() {
+	LittleLemonAndroidTheme {
+		Header()
 	}
 }
